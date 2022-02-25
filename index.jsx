8990daea5a16b662bb2a121dd330fd875c8bfba8
elisefonnes <fonneselise@gmail.com>
2022-02-25 19:55:23 +0100

    nh

---
+++ index.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+
+function Application() {
+    return <h1>Kristiania Movie Database</h1>;
+}
+
+ReactDOM.render(
+    <Application/>,
+    document.getElementById("app")
+
+);
