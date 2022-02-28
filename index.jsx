a7085327779f45f77b2fe729a96b8b7db8d18e01
elisefonnes <fonneselise@gmail.com>
2022-02-28 12:53:32 +0100

    nh

--- index.jsx
+++ index.jsx
@@ -2,8 +2,16 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 
-function Application() {
+function FrontPage() {
     return <h1>Kristiania Movie Database</h1>;
+}
+
+function Application() {
+    return
+        <Router>
+
+         </Router>
+    <FrontPage/>;
 }
 
 ReactDOM.render(
