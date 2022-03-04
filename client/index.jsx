a08e6d237d1a0f412f92bd39a3da8280aef8d382
attichouse <77102070+attichouse@users.noreply.github.com>
2022-03-04 16:35:09 +0100

    removed unused stuff

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QuizGame } from "./quizGame";
 
-ReactDOM.render(<QuizGame />, document.getElementById("app"));
+ReactDOM.render(
+  <BrowserRouter>
+    <QuizGame />
+  </BrowserRouter>,
+  document.getElementById("app")
+);
